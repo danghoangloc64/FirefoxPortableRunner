669b13313cfb7f9bb82b201296403bad291e5a3f
Dang Hoang Loc <danghoangloc>
2021-04-27 23:51:55 +0700

    up

---
+++ HuyDangKyFirefox/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuyDangKyFirefox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadRegisterKey.AddRegisterByKey("FirefoxRunning", "");
+        }
+    }
+}
